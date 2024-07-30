21c0eca40c8a4af5e66213a3bb5273ffe5383460
Pietra Opulinski Oliveira <112398307+piopulinski@users.noreply.github.com>
2024-07-30 09:56:01 -0300

    30/07

--- script.js
+++ script.js
@@ -32,41 +32,42 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Ryle é nutricionista?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "positivo"
+                texto: "verdadeiro",
+                afirmacao: "errado"
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "negativo"
+                texto: "falso",
+                afirmacao: "certo"
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Foi Atlas quem disse: "Você pode parar de nadar agora, Lily,finalmente alcançamos a costa.?",
+?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "positivo"
+                texto: "verdadeiro",
+                afirmacao: "certo"
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "negativo"
+                texto: "falso",
+                afirmacao: "errado"
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "O nome da coléga de quarto da Lily é Alyssa? ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "positivo"
+                texto: "verdadeiro",
+                afirmacao: "errado"
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "negativo"
+                texto: "falso",
+                afirmacao: "certo"
             }
         ]
     },
